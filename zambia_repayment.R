8dc85c769450155bae9de90f52880c64260583fd
Bernard <bernard.kiprop@oneacrefund.org>
2017-10-04 14:50:56 +0300

    Commit 2:    - Fix package installation and attaching to workspace    - Data upload

--- zambia_repayment.R
+++ zambia_repayment.R
@@ -13,16 +13,31 @@ libs <- c("dplyr", "tibble", "reshape2", "ggplot2")
 for(i in 1:length(libs)) {
  if(!require(libs[i])) {
   install.packages(libs[i], dependencies = T)
-  require(libs[i])
+  #require(libs[i])
  }
- require(libs[i])
+ #require(libs[i])
 }
+lapply(libs, "require", character.only = T)
 
 #### Functions ####
 
 
 
 #### Data prep ####
+## Seasons clients data
+sc16r <- read.csv(paste(dd, "Season Clients Detailed_2016.csv", sep = "/"),
+ stringsAsFactors = F)
+sc17r <- read.csv(paste(dd, "Season Clients Detailed_2017.csv", sep = "/"),
+ stringsAsFactors = F)
+
+#Select variables of interest from each df:
+sc16 <- sc16r %>% mutate(
+ 
+)
+
+## Vertical repayment data
+vr16r <- read.csv(paste(dd, "vertical_2016.csv", sep = "/"), stringsAsFactors = F)
+vr17r <- read.csv(paste(dd, "vertical_2017.csv", sep = "/"), stringsAsFactors = F)
 
 
 
@@ -31,6 +46,32 @@ for(i in 1:length(libs)) {
 
 
 #### Time series graphs ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
